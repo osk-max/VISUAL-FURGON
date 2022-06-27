938ada89060fe4140982b9348c84c766ba0a4713
Oscar Cantor <ocantorb1889@gmail.com>
2022-06-27 11:02:04 +0200

    Creando datos ecnanib dom mes form

---
+++ app/app.js
@@ -0,0 +1,31 @@
+/*
+
+INTENTO DE TRABAJAR POO PERO NO TENGO QUE INGRESAR DATOS COMO PARAMETROS, PERO SI DEBE OBTENER EL MES DE UNA API O DEL SERVIDOE E IMPRIMILA EN EL DOM PARA TENER MES ACTUALIZADO - COMCLUSION VOY QUE IMPRIMIO EL ARGUMENTO MESA PERO NO LOGRE CONECTAR CON EL DOMPARA IMPRIMIR EL ID MmesImpresion en el DOM
+
+// POO - LAS CLASES SON FUNCIONES QUE TIENEN DENTRO LOS ARGUMENTOS Y FUNCIONES QUE LOS ACEPTAN
+
+class CambioDeDiferentesFechas {
+  constructor(mesA) {
+    this.divFechAct = mesA;
+    // Date.prototype.getMonth()
+
+    // Imprimir algo en ese campo con el DOM
+    
+    
+    
+  }
+  
+  mes() {
+    console.log(divFechAct);    
+    const mesImpresion = document.querySelector('#mesTitulo');
+    console.log(mesImpresion);    
+
+  }
+}
+
+const mesActual = new CambioDeDiferentesFechas('December')
+
+console.log(mesActual)
+*/
+
+
